511526165c1c8c82da84e617c8d0db2b4fb07622
Gourav <gouravsharma20a@gmail.com>
2024-07-02 15:09:17 +0530

    ANDA KHAO SEHAT BNAO

---
+++ src/anda/anda.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Anda(){
+    return (
+        <div>
+            <h1>AAO sikhaye tumhey andey kaa fanda</h1>
+            <button>PHOD DU KYA </button>
+        </div>
+    );
+
+}
+export default Anda;
